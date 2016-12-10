a7f60c692905bdc95c08b08970ba08dacfc3b187
Jonathan Stowell <stowell.jonathan@gmail.com>
2016-12-10 19:38:30 +0000

    Initial Commit.

---
+++ Cake.RestSharp/RestSharpAlias.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.Diagnostics;
+using RestSharp;
+
+namespace Cake.RestSharp
+{
+    [CakeAliasCategory("RestSharp")]
+    public static class RestSharpAlias
+    {
+        [CakeMethodAlias]
+        public static string GetUriResource(
+            this ICakeContext context,
+            string uri)
+        {
+            var client = new RestClient(uri);
+
+            var request = new RestRequest(Method.GET);
+
+            var response = client.Execute(request);
+
+            context.Log.Information($"Request to uri: {uri} code: {response.StatusCode}");
+
+            return response.StatusCode.ToString();
+        }
+
+        [CakeMethodAlias]
+        public static string GetUriResource(
+            this ICakeContext context,
+            string uri,
+            string resource)
+        {
+            var client = new RestClient(uri);
+
+            var request = new RestRequest(resource, Method.GET);
+
+            var response = client.Execute(request);
+
+            context.Log.Information($"Request to uri: {uri} resource: {resource} code: {response.StatusCode}");
+
+            return response.StatusCode.ToString();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Cake.RestSharp
+
+A Cake Addin for [Rest Sharp](http://restsharp.org/).
+
+[![cakebuild.net](https://img.shields.io/badge/WWW-cakebuild.net-blue.svg)](http://cakebuild.net/)
+
+## Functionality
+
+Supports making GET requests using RestSharp
